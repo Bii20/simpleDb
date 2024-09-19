0ebdc06e3ed60e5d866f24e35d20949917e2b52e
Bii20 <leocheruiyot.bc@gmail.com>
2024-09-19 16:18:22 +0300

    implemented main function

--- simpleDb.c
+++ simpleDb.c
@@ -1,6 +1,24 @@
 #include<stdio.h>
 
-int main(void){
-    printf("Hello DB2!\n");
+int main(int argc ,char*argv[]){
+    InputBuffer* inputBuffer = newInputBuffer();
+
+    while (true)
+    {
+    
+        /* code print*/
+        printPrompt();
+        readInput(inputBuffer);
+        if (strcmp(inputBuffer -> buffer, ".exit")== 0)
+        {
+            /* code */
+            closeInputBuffer(inputBuffer);
+            exit(EXIT_SUCCESS);
+        }else{
+            printf("Unrecognises commad %s .\n",inputBuffer->buffer);
+        }
+        
+    }
+    
 
 } 
